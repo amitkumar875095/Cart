ce7c38c6bb903c39a1144e583e8c5e7555210fd0
Amit <amitkumar.ug21@nsut.ac.in>
2024-03-28 22:09:01 +0530

    add delete increase or decrease from cart

--- src/Cart.js
+++ src/Cart.js
@@ -27,13 +27,50 @@ class Cart extends  React.Component{
        // this.increase = this.increase.bind(this);
       //  this.testing();
       }
+      increase = (product) =>{
+          console.log("increse the qty",product)
+          const {products} = this.state;
+          const index = products.indexOf(product);
+
+          products[index].qty += 1;
+          this.setState ({
+            products : products,
+          })
+      }
+       decrease = (product) =>{
+        console.log("decrease the qty",product);
+         const {products} = this.state;
+         const index = products.indexOf(product);
+         {if(products[index].qty==0){
+              return 
+         }}
+         products[index].qty -=1;
+         this.setState({
+            products
+         })
+    }
+    delete = (id)=>{
+        console.log("delete" , id)
+        const {products} = this.state;
+       
+        const items = products.filter((item)=>item.id != id);
+        this.setState({
+            products : items,
+        })
+    }
     render(){
         const {products} = this.state;
         return (
             <div className='cart'>
          
        {     products.map((items) => {
-              return    <CartItem items = {items} key={items.id}/>
+              return    <CartItem 
+              items = {items} 
+              key={items.id}
+              increase = {this.increase}
+              decrease = {this.decrease}
+              delete = {this.delete}
+              />
             })}
             </div>
         )
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,80 +1,6 @@
 import React from 'react'
 
 class CartItem extends React.Component{
-  
-   // this.increase = this.increase.bind(this);
-  //  this.testing();
-  
- 
-  // testing=  ()=>{
-  // var promise = new Promise((resolve,reject)=>{
-  //   setTimeout(()=>{
-      
-  //     resolve('done');
-  //   },5000);
-  // })
-  // promise.then(()=>{
-     
-
-  //  this.setState({qty : this.state.qty+10});
-
-  //   this.setState({qty : this.state.qty+10});
-  //   console.log('state',this.state);
-  // })
-
-  // }
-  
-  
-  increase = ()=>{
-    // this.state.qty += 1;\\
-             /// set state also have another call back function ans it is a asynchronous 
-
-    //set state form 1 
-    // shallow mergin change state qty and react render automatically
-    //if preState not required use this
-    //  this.setState({
-    //    qty : this.state.qty + 1,
-    //  })
-
-     //set state form 2
-    // pass a call back function and pass a previous state to call back funtion
-    // when previous state required to increase
-      this.setState((preState)=>{
-           return {
-            //shallow mergin
-
-            qty : preState.qty + 1,
-           }
-      },()=>{
-        console.log(this.state);
-      })
-
-    console.log("test",this.state);
-  }
-
-  
-  //Arrow function bind this value to the class
-  // increase(){
-  //    console.log("test",this.state);
-  // }
-
-
-
-  // decrease arrow function to bind to the class
-   decrease = ()=>{
-    const {qty} = this.state;
-   {if(qty==0){
-       return ;
-    }}
-   this.setState (
-     //prestate used,
-     (preState)=>{
-      return {
-        qty : preState.qty-1,
-      }
-     }
-   )
-   }
 render(){
   {console.log("this props",this.props)};
   const {price,title,qty} = this.props.items;
@@ -93,9 +19,9 @@ render(){
             // button
             }
            
-             <img alt = "increase" className = "action-icons" src = "https://cdn-icons-png.flaticon.com/512/2997/2997933.png" onClick = {this.increase.bind(this)} />
-             <img alt = "decrease" className = "action-icons" src = "https://cdn-icons-png.flaticon.com/512/1828/1828899.png" onClick = {this.decrease}/>
-             <img alt = "delete" className = "action-icons" src = "https://cdn-icons-png.flaticon.com/512/1214/1214428.png" />
+             <img alt = "increase" className = "action-icons" src = "https://cdn-icons-png.flaticon.com/512/2997/2997933.png" onClick = {()=>this.props.increase(this.props.items)} />
+             <img alt = "decrease" className = "action-icons" src = "https://cdn-icons-png.flaticon.com/512/1828/1828899.png" onClick = {()=>this.props.decrease(this.props.items)}/>
+             <img alt = "delete" className = "action-icons" src = "https://cdn-icons-png.flaticon.com/512/1214/1214428.png"  onClick = {()=>this.props.delete(this.props.items.id)}  />
              </div>
            </div>
         </div>
